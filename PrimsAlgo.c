012db95f14b7b2e017f54de95f32e12ee4d1fcda
SumitPaul22 <140430502+SumitPaul22@users.noreply.github.com>
2024-04-04 11:05:37 +0530

    Update PrimsAlgo.c

--- PrimsAlgo.c
+++ PrimsAlgo.c
@@ -4,36 +4,35 @@
 #define MAX 10
 #define INF 999
 
-int b, u, v, n = 5, i, j, ne = 1;
-int visited[MAX] = {0}, min, mincost = 0, a;
-
-int cost[MAX][MAX] = {
-    {INF, 2, INF, 6, INF},
-    {2, INF, 3, 8, 5},
-    {INF, 3, INF, INF, 7},
-    {6, 8, INF, INF, 9},
-    {INF, 5, 7, 9, INF}
-};
-
 int main() {
-    visited[1] = 1; // Start from node 1
+    int b, u, v, n = 5, i, j, ne = 1;
+    int visited[MAX] = {0}, min, mincost = 0, a;
+
+    int cost[MAX][MAX] = {
+        {INF, 2, INF, 6, INF},
+        {2, INF, 3, 8, 5},
+        {INF, 3, INF, INF, 7},
+        {6, 8, INF, INF, 9},
+        {INF, 5, 7, 9, INF}
+    };
+
+    // Start from node 0 for indexing consistency
+    visited[0] = 1;
     printf("The edges of Minimum Cost Spanning Tree are\n");
     
     while(ne < n) {
         for(i = 0, min = INF; i < n; i++) {
             for(j = 0; j < n; j++) {
-                if(cost[i][j] < min && visited[i] != 0) {
+                if(cost[i][j] < min && visited[i] != 0 && visited[j] == 0) {
                     min = cost[i][j];
                     a = u = i;
                     b = v = j;
                 }
             }
         }
-        if(visited[u] == 0 || visited[v] == 0) {
-            printf("\nEdge %d:(%d %d) cost:%d", ne++, a+1, b+1, min);
-            mincost += min;
-            visited[b] = 1;
-        }
+        printf("\nEdge %d:(%d %d) cost:%d", ne++, a+1, b+1, min);
+        mincost += min;
+        visited[b] = 1;
         cost[a][b] = cost[b][a] = INF; 
     }
     printf("\nMinimum cost = %d\n", mincost);

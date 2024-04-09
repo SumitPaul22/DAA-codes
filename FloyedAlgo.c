4ef2f245a9d00e49a02ee8344437da64770e092c
SumitPaul22 <140430502+SumitPaul22@users.noreply.github.com>
2024-04-10 00:42:18 +0530

    Update FloyedAlgo.c

--- FloyedAlgo.c
+++ FloyedAlgo.c
@@ -11,16 +11,12 @@ int graph[MAX][MAX] = {
     {17, 27, 8, 0, 3},
     {3, 2, 5, 3, 0}
 };
-int i, j, k, n = 5; // n is the number of vertices in the graph
+int i, j, k, n = 5;
 
 void floydWarshall() {
-    // Using k for intermediate vertices
     for (k = 0; k < n; k++) {
-        // Pick all vertices as source one by one
         for (i = 0; i < n; i++) {
-            // Pick all vertices as destination for the above picked source
             for (j = 0; j < n; j++) {
-                // If vertex k is on the shortest path from i to j, then update the value of graph[i][j]
                 if (graph[i][k] + graph[k][j] < graph[i][j])
                     graph[i][j] = graph[i][k] + graph[k][j];
             }

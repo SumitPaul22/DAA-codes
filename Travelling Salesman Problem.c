aa4308cd775af95b11f5e0a77c42f027331b83da
SumitPaul22 <140430502+SumitPaul22@users.noreply.github.com>
2024-04-26 00:02:29 +0530

    Update Travelling Salesman Problem.c

--- Travelling Salesman Problem.c
+++ Travelling Salesman Problem.c
@@ -1,72 +1,53 @@
 #include <stdio.h>
-#include <limits.h>
 
-#define N 4
-#define VISITED_ALL (1<<N) - 1
-
-int dist[N][N] = {
+int tsp[10][10] = {
     {0, 400, 500, 300},
     {400, 0, 300, 500},
     {500, 300, 0, 400},
     {300, 500, 400, 0}
 };
 
-int dp[16][4];
-int next[16][4];
+int visited[10], n, cost = 0;
 
-int min(int a, int b) {
-    return a < b ? a : b;
-}
+void travellingsalesman(int c) {
+    int k, adj_vertex = 999;
+    int min = 999;
 
-int tsp(int mask, int pos) {
-    if(mask == VISITED_ALL) {
-        return dist[pos][0];
-    }
-    if(dp[mask][pos] != -1) {
-       return dp[mask][pos];
-    }
+    visited[c] = 1;
+    printf("%d ", c + 1);
 
-    int ans = INT_MAX;
-    int cityIndex = -1;
-
-    // Try to go to an unvisited city
-    for(int city = 0; city < N; city++) {
-        if((mask & (1 << city)) == 0) {
-            int newAns = dist[pos][city] + tsp(mask | (1 << city), city);
-            if(newAns < ans) {
-                ans = newAns;
-                cityIndex = city;
+    for (k = 0; k < n; k++) {
+        if (tsp[c][k] != 0 && !visited[k]) {
+            if (tsp[c][k] < min) {
+                min = tsp[c][k];
             }
+            adj_vertex = k;
         }
     }
 
-    next[mask][pos] = cityIndex;
-    return dp[mask][pos] = ans;
-}
+    if (min != 999) {
+        cost += min;
+    }
 
-void findPath(int mask, int pos) {
-    if(mask == VISITED_ALL) {
-        printf("A");
+    if (adj_vertex == 999) {
+        adj_vertex = 0;
+        printf("%d", adj_vertex + 1);
+        cost += tsp[c][adj_vertex];
         return;
     }
 
-    int nextCity = next[mask][pos];
-    printf("%c-->", 'A' + nextCity);
-    findPath(mask | (1 << nextCity), nextCity);
+    travellingsalesman(adj_vertex);
 }
 
 int main() {
-    for(int i = 0; i < (1<<N); i++) {
-        for(int j = 0; j < N; j++) {
-            dp[i][j] = -1;
-            next[i][j] = -1;
-        }
+    int i;
+    n = 5;
+    for (i = 0; i < n; i++) {
+        visited[i] = 0;
     }
 
-    printf("The minimum cost is %d\n", tsp(1, 0));
-    printf("The path is: A-->");
-    findPath(1, 0);
-    printf("\n");
-
+    printf("Shortest Path: ");
+    travellingsalesman(0);
+    printf("\nMinimum Cost: %d\n", cost);
     return 0;
 }

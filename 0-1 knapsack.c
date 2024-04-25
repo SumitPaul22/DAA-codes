2cdcd198d00e246236faddbeb74963a61ddcdea8
SumitPaul22 <140430502+SumitPaul22@users.noreply.github.com>
2024-04-25 20:28:41 +0530

    Update 0-1 knapsack.c

--- 0-1 knapsack.c
+++ 0-1 knapsack.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
+int max(int a,int b){
+    return (a>b)? a:b;
 }
 
-int main() {
-    int n, w, i, j;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    int v[n], wt[n];
-    printf("Enter the weight of knapsack: ");
-    scanf("%d", &w);
-
-    for (i = 0; i < n; i++) {
-        printf("Enter the weight and value: ");
-        scanf("%d %d", &wt[i], &v[i]);
+int main(){
+    int n,w,i,j;
+    printf("Enter the no. of elements: ");
+    scanf("%d",&n);
+    int wt[n],v[n];
+    printf("Enter the weight of the knapsack: ");
+    scanf("%d",&w);
+    for (i=0;i<n;i++){
+        printf("Enter the weight and profit: ");
+        scanf("%d %d",&wt[i],&v[i]);
     }
-
-    int p[w + 1];
-    for (i = 0; i <= w; i++)
-        p[i] = 0;
-
-    for (i = 0; i < n; i++) {
-        for (j = w; j >= wt[i]; j--)
-            p[j] = max(p[j], p[j - wt[i]] + v[i]);
+    int p[w+1];
+    for (i=0;i<w;i++){
+        p[i]=0;
     }
-
-    printf("Maximum value: %d\n", p[w]);
-
-    printf("Chosen weights and values:\n");
-    j = w;
-    for (i = n - 1; i >= 0; i--) {
-        if (p[j] != p[j - wt[i]] + v[i]) continue;
-        printf("  - Weight: %d, Value: %d\n", wt[i], v[i]);
-        j -= wt[i];
+    for (i=0;i<n;i++){
+        for(j=w;j>=wt[i];j--){
+            p[j]=max(p[j],p[j-wt[i]]+v[i]);
+        }
     }
-
+    printf("Chosen Weights and Profit are: \n");
+    j=w;
+    for(i=n-1;i>=0;i--){
+        if(p[j]!=p[j-wt[i]]+v[i]) continue;
+            printf(" - Weight: %d\tProfit:%d\n",wt[i],v[i]);
+        j-=wt[i];
+    }
+    printf("Maximum Profit: %d",p[w]);
     return 0;
 }
